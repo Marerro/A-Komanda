eba4e14267a1a325b7cad204898c6f8fbfff4932
vejas valiukonis <vejasva@gmail.com>
2024-12-05 10:38:05 +0200

    formatting

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,12 +10,22 @@ export default function Navigation() {
   return (
     <>
       <div className="flex flex-row m-2">
-        <Link to="/"><img src={logo} alt="logo" /></Link>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
         <div className="flex flex-row">
-            <Link to="/home"><img className="m-2" src={home} alt="home" /></Link>
-            <Link to="/movies"><img className="m-2" src={movies} alt="movies" /></Link>
-            <Link to="/tvseries"><img className="m-2" src={tvSeries} alt="tv series" /></Link>
-            <Link to="/bookmarks"><img className="m-2" src={bookmark} alt="bookmarks" /></Link>
+          <Link to="/home">
+            <img className="m-2" src={home} alt="home" />
+          </Link>
+          <Link to="/movies">
+            <img className="m-2" src={movies} alt="movies" />
+          </Link>
+          <Link to="/tvseries">
+            <img className="m-2" src={tvSeries} alt="tv series" />
+          </Link>
+          <Link to="/bookmarks">
+            <img className="m-2" src={bookmark} alt="bookmarks" />
+          </Link>
         </div>
         <img src={avatar} alt="user picture" />
       </div>
