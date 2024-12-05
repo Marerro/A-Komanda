f2a4e01a7c70b5ae0d31c0954fce6ed2f4b6420b
D1JS <kuneckisj@gmail.com>
2024-12-05 14:12:14 +0300

    Recommended section functionality

--- src/components/RecommendedForYou.jsx
+++ src/components/RecommendedForYou.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { getAll } from "../helpers/get";
+import category_icon from "@assets/icon-category-movie.svg";
 
 function RecommendedForYou() {
   const [recommendMovies, setRecommendMovies] = useState([]);
+
+  console.log(category_icon);
 
   const url = "http://localhost:5000/data";
 
@@ -13,7 +16,7 @@ function RecommendedForYou() {
       console.log("Gauti duomenys sekmingai", data);
       setRecommendMovies(data);
     } catch (error) {
-      console.error("Duomenys nebuvo išgauti iš endpoint", error);
+      console.error("Duomenys nebuvo gauti iš endpoint", error);
     }
   };
 
@@ -26,31 +29,45 @@ function RecommendedForYou() {
       <div className="recommended_container">
         <h3>Recommended for you</h3>
       </div>
-      {recommendMovies.map((itemData) => {
-        const {
-          id,
-          title,
-          thumbnail,
-          trending,
-          small,
-          large,
-          regular,
-          year,
-          category,
-          rating,
-          isBookmarked,
-          isTrending,
-        } = itemData;
+      <div className="m-1 grid grid-cols-2">
+        {recommendMovies.map((itemData) => {
+          const {
+            id,
+            title,
+            thumbnail,
+            trending,
+            small,
+            large,
+            regular,
+            year,
+            category,
+            rating,
+            isBookmarked,
+            isTrending,
+          } = itemData;
 
-        return (
-          <div key={id}>
-            <div className="grid grid-cols-2 gap-3">
-              <img src={thumbnail.small} alt="#" />
+          return (
+            <div key={id} className="">
+              <div className="grid grid-cols-2 gap-3 w-[23.4375rem] h-[8.75rem] justify-center">
+                <img
+                  className="rounded"
+                  src={itemData.thumbnail.regular.small}
+                  alt="#"
+                />
+              </div>
+              <div className="flex card_content">
+                <p>{year}</p>
+                <span>&#8226;</span>
+                <img src={category_icon} alt="#" />
+                <p>{category}</p>
+                <span>&#8226;</span>
+                <p>{rating}</p>
+              </div>
+              <p>{title}</p>
             </div>
-            <p>{title}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 }
