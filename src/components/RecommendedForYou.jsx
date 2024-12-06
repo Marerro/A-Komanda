a869855508289047364b54ce715087df6dae1c47
D1JS <kuneckisj@gmail.com>
2024-12-06 12:06:12 +0300

    Changes in SCRUM-21 section

--- src/components/RecommendedForYou.jsx
+++ src/components/RecommendedForYou.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import { getAll } from "../helpers/get";
-import category_icon from "@assets/icon-category-movie.svg";
+import category_movie from "@assets/icon-category-movie.svg";
 import bookmark from "@assets/icon-bookmark-empty.svg";
+import category_TV from "@assets/icon-category-tv.svg";
 
 function RecommendedForYou() {
   const [recommendMovies, setRecommendMovies] = useState([]);
-
-  console.log(category_icon);
 
   const url = "http://localhost:5000/data";
 
@@ -14,7 +13,6 @@ function RecommendedForYou() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log("Gauti duomenys sekmingai", data);
       setRecommendMovies(data);
     } catch (error) {
       console.error("Duomenys nebuvo gauti iš endpoint", error);
@@ -48,37 +46,44 @@ function RecommendedForYou() {
           } = itemData;
 
           return (
-            <>
-              <div className="">
-                <div key={id} className="m-auto relative z-0">
-                  <div className="grid grid-cols-2 gap-[0.94rem] w-[23.4375rem] h-[8.75rem] justify-center ">
-                    <img
-                      className="absolute w-[0.72919rem] h-[0.875rem] top-[0.5rem] right-[0.5rem]"
-                      src={bookmark}
-                      alt="BookmarkIcon"
-                    />
-                    <img
-                      className="rounded-[0.5rem]"
-                      src={itemData.thumbnail.regular.small}
-                      alt="#"
-                    />
-                  </div>
-                  <div className="flex card_content text-[#FFF]/[0.75] gap-[0.5rem;] text-[0.6875rem]">
-                    <p>{year}</p>
-                    <span>&#8226;</span>
+            <div key={id} className="">
+              <div className="m-auto relative z-0">
+                <div className="grid grid-cols-2 gap-[0.94rem] w-[23.4375rem] h-[8.75rem] justify-center  ">
+                  <img
+                    className="absolute w-[0.8919rem] h-[1.375rem] top-[0.5rem] right-[0.5rem] border-solid border-[##10141E] rounded-[50px] opacity-[0.500647] box-border"
+                    src={bookmark}
+                    alt="BookmarkIcon"
+                  />
+                  <img
+                    className="rounded-[0.5rem]"
+                    src={itemData.thumbnail.regular.small}
+                    alt="#"
+                  />
+                </div>
+                <div className="flex card_content text-[#FFF]/[0.75] gap-[0.5rem;] text-[0.6875rem]">
+                  <p>{year}</p>
+                  <span>&#8226;</span>
+                  {category === "Movie" && (
                     <img
                       className="w-[0.625rem] shrink h-[0.625rem]"
-                      src={category_icon}
+                      src={category_movie}
                       alt="#"
                     />
-                    <p>{category}</p>
-                    <span>&#8226;</span>
-                    <p>{rating}</p>
-                  </div>
-                  <p className="text-[#FFF] text-[0.875rem]">{title}</p>
+                  )}
+                  {category === "TV Series" && (
+                    <img
+                      className="w-[0.625rem] shrink h-[0.625rem]"
+                      src={category_TV}
+                      alt="#"
+                    />
+                  )}
+                  <p>{category}</p>
+                  <span>&#8226;</span>
+                  <p>{rating}</p>
                 </div>
+                <p className="text-[#FFF] text-[0.875rem]">{title}</p>
               </div>
-            </>
+            </div>
           );
         })}
       </div>
