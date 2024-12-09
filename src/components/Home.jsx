b3c511c9ef439ab87504950f0b140bc60f8b692d
D1JS <kuneckisj@gmail.com>
2024-12-09 16:33:14 +0300

    Searchbar functionallity done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Navigation from "./Navigation";
 import RecommendedForYou from "./RecommendedForYou";
 import Trending from "./Trending";
 import Searchbar from "./Searchbar";
 
 export default function Home() {
+  const [showComponent, setShowComponent] = useState(false);
+
   return (
     <>
       <Navigation />
-      <Searchbar />
-      <Trending />
-      <RecommendedForYou />
+      <Searchbar
+        showComponent={showComponent}
+        setShowComponent={setShowComponent}
+      />
+
+      {!showComponent && <Trending />}
+      {!showComponent && <RecommendedForYou />}
     </>
   );
 }
