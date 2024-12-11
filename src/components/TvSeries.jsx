a8be21fefa2d49579b1e29397d21213f90465320
Darius1989 <darius.alksnis@gmail.com>
2024-12-11 13:00:11 +0200

    add figma css

--- src/components/TvSeries.jsx
+++ src/components/TvSeries.jsx
@@ -5,8 +5,9 @@ import category_TV from "@assets/icon-category-tv.svg";
 import bookmarkIconEmpty from "@assets/icon-bookmark-empty.svg";
 import bookmarkIconFull from "@assets/icon-bookmark-full.svg";
 import Navigation from "./Navigation";
-import SearchBar from "./SearchBar"
+import SearchBar from "./Searchbar"
 import bookmarkIconHover from "@assets/icon-bookmark-hover.svg";
+import playIcon from "@assets/icon-play.svg";
 
 
 
@@ -37,10 +38,10 @@ export default function TvSeries() {
     <section className="mx-[1rem]">
       <Navigation />
       <SearchBar />
-      <div className="">
-        <h3 className="heading-xs tablet:heading-m desktop:heading-m my-[1.5rem]">TV Series </h3>
+      <div>
+        <h3 className="heading-xs tablet:heading-m desktop:heading-m my-[2.375rem] tracking-wide">TV Series </h3>
       </div>
-      <div className="bg-[#10141E]  grid grid-cols-2 tablet:grid-cols-3 desktop:grid-cols-4  gap-[0.94rem] ">
+      <div className="bg-[#10141E]  grid grid-cols-2 tablet:grid-cols-3 desktop:grid-cols-4 desktop:gap-[2.5rem] tablet:gap-[2.5rem] mobile:gap-[0.94rem]">
         {tvseries.map((itemData) => {
           const {
             id,
@@ -98,27 +99,31 @@ export default function TvSeries() {
 
             <div key={id} className="justify-self-center">
               <div className="m-auto relative z-0">
-                <div className="">
+                <div className="group/play relative">
+                <div className="group-hover/play:visible invisible flex justify-around absolute bg-white/25 rounded-full w-[7.25rem] h-11 m-auto top-0 bottom-0 left-0 right-0">
+                  <img className="m-2 w-7 h-7" src={playIcon} />
+                  <p className="heading-xs mt-2 mb-2 ml-5 mr-5">Play</p>
+                </div>
                   <picture>
                     <source
                       media="(min-width: 768px)"
                       srcSet={itemData.thumbnail.regular.large}
                     />
                     <img
-                      className=""
+                      className="rounded-lg"
                       src={itemData.thumbnail.regular.small}
                       alt="TV shows"
                     />
                   </picture>
 
                 </div>
-                <div className="flex  gap-[0.44rem;] body-s tablet:body-s desktop:body-s mt-[0.5rem]">
+                <div className="flex  gap-[0.25rem] tablet:gap-[0.5rem] desktop:tablet:gap-[0.5rem] body-s tablet:body-s desktop:body-s mobile:mt-[0.25rem] tablet:mt-[0.31rem] desktop:tablet:mt-[0.31rem] opacity-75 text-[0.6875rem] tablet:text-[0.8125rem] desktop:text-[0.8125rem]">
                   <p>{year}</p>
                   <span>&#8226;</span>
 
                   {category === "TV Series" && (
                     <img
-                      className="w-[0.625rem] shrink h-[0.625rem] tablet:w-[0.72919rem]  translate-y-[0.3rem]"
+                      className="w-[0.625rem]  h-[0.625rem] tablet:w-[0.75rem] tablet:h-[0.75rem] desktop:w-[0.75rem] desktop:h-[0.75rem] mobile:translate-y-[0.20rem] translate-y-[0.3rem]"
                       src={category_TV}
                       alt="#"
                     />
@@ -128,7 +133,7 @@ export default function TvSeries() {
                   <span>&#8226;</span>
                   <p>{rating}</p>
                 </div>
-                <p className="section-s mt-[0.25rem] tablet:heading-xs desktop:heading-xs">{title}</p>
+                <p className="mobile:mt-[0.25rem] tablet:mt-[0.31rem] desktop:tablet:mt-[0.31rem] section-s mobile:section-s tablet:heading-xs desktop:heading-xs">{title}</p>
               </div>
             </div>
           );
