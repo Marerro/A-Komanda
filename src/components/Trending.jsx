f43fc569e273b699d39f86737e64338205e7b438
henryk <henryk.umbrasevic@stud.techin.lt>
2024-12-16 11:47:00 +0400

    trending section's CSS don for all screens. Search Bar CSS adjusted

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -32,16 +32,16 @@ const Trending = () => {
   return (
     <>
       <div>
-        <h1 className="mobile:heading-xs tablet:heading-l text-[1.25rem] mobile:pl-[1rem] tablet:pl-[1.56rem] desktop:pl-[2.35rem] tracking-[0.05rem] trending-title mobile:mt-[1.4rem] tablet:mt-[0.9rem] tablet:tracking-[0.00215rem] mobile:mb-[1rem] tablet:mb-[1.3rem] desktop:mb-[1.4rem]">
+        <h1 className="heading-xs mobile:heading-xs tablet:heading-l text-[1.25rem] pl-[1rem] mobile:pl-[1rem] tablet:pl-[1.56rem] desktop:pl-[2.35rem] tracking-[0.05rem] mobile:mt-[1.4rem] tablet:mt-[0.9rem] tablet:tracking-[0.00215rem] mb-[1rem] mobile:mb-[1rem] tablet:mb-[1.3rem] desktop:mb-[1.4rem]">
           Trending
         </h1>
       </div>
       <div
-        className="bg-[#10141E] mobile:pl-[1rem] tablet:pl-[1.56rem] desktop:pl-[2.25rem] overflow-x-auto no-scrollbar"
+        className="bg-[#10141E] pl-[1rem] mobile:pl-[1rem] tablet:pl-[1.56rem] desktop:pl-[2.25rem] overflow-x-auto no-scrollbar"
         {...events}
         ref={ref}
       >
-        <div className="flex mobile:gap-[1rem] tablet:gap-[2.5rem]">
+        <div className="flex gap-[1rem] mobile:gap-[1rem] tablet:gap-[2.5rem]">
           {data.map((show) => {
             const {
               id,
@@ -66,7 +66,7 @@ const Trending = () => {
               setUpdate((prev) => prev + 1);
             };
 
-            const bookMarking = isBookmarked ? <button onClick={() => unBookmark(id)} className="absolute mobile:top-[0.5rem] mobile:right-[0.5rem] tablet:top-[1rem] tablet:right-[1.5rem] bg-dark-blue/50 w-8 h-8 rounded-full group/bookmark z-50">
+            const bookMarking = isBookmarked ? <button onClick={() => unBookmark(id)} className="absolute top-[0.5rem] right-[0.5rem] mobile:top-[0.5rem] mobile:right-[0.5rem] tablet:top-[1rem] tablet:right-[1.5rem] bg-dark-blue/50 w-8 h-8 rounded-full group/bookmark z-50">
               <img className="m-auto group-hover/bookmark:invisible" src={bookmarkIconFull} />
               <img className="m-auto invisible group-hover/bookmark:visible absolute top-0 right-0" src={bookmarkIconHover} />
             </button> : <button onClick={() => bookMark(id)} className="absolute mobile:top-[0.5rem] mobile:right-[0.5rem] tablet:top-[1rem] tablet:right-[1.5rem] bg-dark-blue/50 w-8 h-8 rounded-full group/bookmark z-50">
@@ -83,7 +83,7 @@ const Trending = () => {
                       srcSet={thumbnail.trending.large} />
                     <img
                       className="max-w-[29.375rem] max-h-[14.375rem]
-                      mobile:w-[15rem] mobile:h-[8.75rem] tablet:w-[29.375rem] tablet:h-[14.375rem]
+                      w-[15rem] h-[8.75rem] mobile:w-[15rem] mobile:h-[8.75rem] tablet:w-[29.375rem] tablet:h-[14.375rem]
                        rounded-[0.5rem] group-hover/play:opacity-50 hover:duration-500"
                       src={thumbnail.trending.small}
                       alt="TV shows"/>
@@ -101,7 +101,7 @@ const Trending = () => {
                     <p className="text_above_title mobile:body-s mobile:mx-[0.4rem] tablet:mx-[0.5rem] tablet:body-m">&#8226;</p>
                     <p className="text_above_title mobile:body-s mobile:mr-[0.5rem] tablet:body-m tablet:mr-[5rem]">{rating}</p>
                     <div className="absolute mobile:top-[1.2rem] tablet:top-[1.4rem]">
-                      <p className="mobile:body-m tablet:heading-m movie-title">{title}</p>
+                      <p className="body-m mobile:body-m tablet:heading-m movie-title">{title}</p>
                     </div>
                   </div>
                 </div>
