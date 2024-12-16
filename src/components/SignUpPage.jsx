19f0f6c247af3b6361cdb5845e507fd3c5131247
laima <laima.kulbyte@stud.techin.lt>
2024-12-16 10:41:00 +0400

    comments in Sign up page

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -22,12 +22,15 @@ const SignUpPage = () => {
     const userData = { email: data.email, password: data.password };
     try {
       const users = await getAllUsers();
+      // Finds out if the user's email exists
       const existingUser = users.find((user) => user.email === userData.email);
       if (existingUser) {
+        // If the user with such an email exists, shows an error and resets the form
         setError(`User with such an email already exist`);
         reset();
         return;
       } else {
+        // If entered email is new , adds user to database, updates it's state and navigates to Log in page
         const newUser = await post(userData);
         setUsers((prev) => [...prev, newUser]);
 
@@ -45,23 +48,27 @@ const SignUpPage = () => {
   return (
     <>
       <div className="flex flex-col place-items-center h-dvh">
+        {/* Icon */}
         <div className="pb-[3.3rem] pt-[3rem] tablet:pt-[5.5rem] tablet:pb-[4.53rem] desktop:pt-[4.9rem] desktop:pb-[5.19rem]">
           <img src={Logo} className="w-[2rem] h-[1.6rem]" />
         </div>
+
         <div className="bg-semi-dark-blue rounded-[0.63rem] pr-[1.5rem] tablet:pr-[2rem] tablet:rounded-[1.25rem] ">
           <h1 className="heading-l pl-[1.5rem] pt-[1.5rem] pb-[2.2rem] tablet:pl-[2rem] tablet:pt-[1.7rem] tablet:pb-[2.1rem] desktop:pb-[2.1rem] desktop:pt-[1.75rem] tracking-tight">
             Sign Up
           </h1>
+          {/* Form */}
+          {/* While submitting the form, calls "onSubmit" function */}
           <form
             onSubmit={handleSubmit(onSubmit)}
             className="pl-[1.5rem] tablet:pl-[2rem]"
           >
+            {/* Email input window */}
             <div className="pb-[1.2rem] tablet:pb-[1.1rem] relative">
               <input
                 type="text"
                 id="email"
                 placeholder="Email address"
-                
                 {...register("email", {
                   required: "Can't be empty",
                   pattern: {
@@ -69,11 +76,15 @@ const SignUpPage = () => {
                     message: "Invalid email adress",
                   },
                 })}
+                // If the error occurs, sets input border red
+                // Tailwind autofill class, leaves original CSS
+                // Calls setError state to remove the error when user starts typing
                 className={`bg-semi-dark-blue border-0 border-b-[0.063rem] pb-7 pl-[1rem] tablet:pb-[1.9rem] desktop:pb-[1.7rem] tablet:w-[21rem] autofill:transition-colors autofill:duration-[999999999s] hover:opacity-100 hover:border-b-white form-input ${
                   errors.email ? "border-red" : "greyish-blue"
                 }`}
                 onInput={() => setError("")}
               />
+              {/* Separation of errors by checking their type and placing them differently*/}
               <div className="relative">
                 {errors.email?.type === "required" && (
                   <p className="error-text text-red absolute bottom-[1.15rem] left-[10.84rem] tablet:left-[14.44rem]">
@@ -84,11 +95,12 @@ const SignUpPage = () => {
               <div className="relative">
                 {errors.email?.type === "pattern" && (
                   <p className="error-text text-red absolute top-[0.1rem] left-[0.5rem]">
-                   Invalid email address
+                    Invalid email address
                   </p>
                 )}
               </div>
             </div>
+            {/* Passwword input window */}
             <div className="pb-[1.3rem] tablet:pb-[1.1rem] desktop:pt-[0.25rem]">
               <input
                 type="password"
@@ -123,6 +135,8 @@ const SignUpPage = () => {
                 )}
               </div>
             </div>
+
+            {/* Repeat password input window */}
             <div className="pb-[2.4rem] tablet:pb-[1.45rem] desktop:pt-[0.2rem]">
               <input
                 type="password"
@@ -157,12 +171,14 @@ const SignUpPage = () => {
               </div>
             </div>
             <div className="relative">
+              {/* Shows errors coming from error state */}
               {error && (
                 <p className="error-text text-red absolute pl-[0.6rem] bottom-[0.2rem]">
                   {error}
                 </p>
               )}
             </div>
+            {/* Submit button */}
             <button
               type="submit"
               className="bg-red text-white body-m rounded-[0.375rem] w-[17.438rem] h-[3rem] hover:bg-white hover:text-semi-dark-blue hover:duration-500 tablet:w-[21rem]"
@@ -171,7 +187,7 @@ const SignUpPage = () => {
             </button>
           </form>
           <p className="body-m pl-[3.7rem] pt-[1.4rem] pb-[1.62rem] tablet:pl-[5.75rem] tablet:pb-[2rem]">
-            Already have an account? 
+            Already have an account?
             <Link to={`/login`} className="text-red body-m mx-1 tablet:mx-3 ">
               Login
             </Link>{" "}
